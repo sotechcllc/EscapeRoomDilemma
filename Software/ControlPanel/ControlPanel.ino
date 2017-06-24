b9dd3d92ff5f55ec4468c6a1ab5217fedbb9448a
Fluvio Lobo <fluvio.lobo@gmail.com>
2017-06-24 17:19:10 -0400

    Added the passwords requested by the client.

--- Software/ControlPanel/ControlPanel.ino
+++ Software/ControlPanel/ControlPanel.ino
@@ -11,6 +11,7 @@
 /// Import Libraries and/or Modules
 #include "KeypadSetup.h"
 #include "Adafruit_RA8875Setup.h"
+#include "ScenarioSetup.h"
 #include "ParseInput.h"
 
 void setup() {
--- Software/ControlPanel/ScenarioSetup.h
+++ Software/ControlPanel/ScenarioSetup.h
@@ -5,4 +5,18 @@
  *
  * Fluvio Lobo Fenoglietto 06/24/2017
  */
- 
+
+/// Scenario Variables
+
+int mode = 0;                       // Initial mode, "want to play?"
+//       = 1;                       // First password
+//       = 2;                       // Second password
+//       = 3;                       // Third password
+//       = 4;                       // Fourth password
+//       = 5;                       // Fifth password
+
+char password_one[3]      = "29";
+char password_two[4]      = "367";
+char password_third[5]    = "2451";
+char password_four[5]     = "*318";
+char password_five[5]     = "###0";
