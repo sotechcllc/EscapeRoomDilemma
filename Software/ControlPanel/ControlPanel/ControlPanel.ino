ee94d564b9d79944b4478b7915d759360e1c6189
Fluvio Lobo <fluvio.lobo@gmail.com>
2017-06-24 15:29:23 -0400

    Created a main Arduino script in which all other functions or services will be integrated.

---
+++ Software/ControlPanel/ControlPanel/ControlPanel.ino
@@ -0,0 +1,19 @@
+/*
+ * Control Panel
+ * 
+ * The following algorithm control lights based on a keypad input visualized on a 5" screen
+ * The algorithm was designed for Chaz Grove at Escape Room Dilemma LLC
+ * 
+ * Fluvio L Lobo Fenoglietto 06/24/2017
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
